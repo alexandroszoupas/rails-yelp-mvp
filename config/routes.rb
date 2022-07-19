9168a16c8f65278fa463b40c19675087798597e2
Alexandros Zoupas <alexandros.p.zoupas@gmail.com>
2022-07-19 19:59:35 +0300

    completed functions in the restaurants controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  # read all
+  get 'restaurants', to: 'restaurants#index'
+
+  # create one
+  get '/restaurants/new', to: 'restaurants#new'
+  post '/restaurants', to: 'restaurants#create'
+
+  # read one
+  get 'restaurants/:id', to: 'restaurants#show'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
