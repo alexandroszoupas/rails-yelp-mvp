b1699da89345a261c12c78cda325d8becd4e4e3a
Alexandros Zoupas <alexandros.p.zoupas@gmail.com>
2022-07-19 19:37:55 +0300

    models restaurant and reviews created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Rails.application.config.assets.paths << Rails.root.join("node_modules")
